5a803d5b495896317047393a8e04f6ecfa0a61e4
nisnym <nisnym@gmail.com>
2021-08-21 23:07:42 +0530

    Beta Day 01

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,7 +1,13 @@
 // Your First Program
 
 class HelloWorld {
+    
+    public static void printHe(){
+        System.out.println("Kya ji");
+    }    
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        printHe(); 
     }
+
 }
---
+++ Java/oddEven.java
@@ -0,0 +1,13 @@
+public class oddEven {
+    public static void main(String[] args) {
+        System.out.println("Java Programming ☕ ");
+        String s1 = "Hello";
+        String s2 = " World!";
+        String s3 = s1.concat(s2);
+        // concatenates strings s1 and s2
+        System.out.println(s3);
+    }
+}
+
+
+//A Java program must have one class whose name is the same as the program filename.
