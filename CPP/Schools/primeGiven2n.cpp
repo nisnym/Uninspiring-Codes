bbed29de962b047feda2f99389b6723c7e695c95
nisnym <nisnym@gmail.com>
2021-08-31 23:29:02 +0530

    Fn + Nostalgic

---
+++ CPP/Schools/primeGiven2n.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+main(){
+    int num, i, Flag=0,N;
+    cout<<"ENter the numver to ";
+    cin>>N;
+    cout<<"The numbers are ";
+    for (num = 1 ; num <= N ; num++){
+        Flag = 0;
+        for (int i = 2; i <= num/2; i++)
+        {
+            if(num%i == 0){
+                Flag = 1;
+                break;
+            }
+        }
+        if(Flag == 0 && num!= 1)
+            cout<< num << setw(3);
+    }
+}
