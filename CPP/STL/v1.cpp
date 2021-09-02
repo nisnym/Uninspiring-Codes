76c35c03f760d80a1385d0eb7bde3e50b0dcffcc
nisnym <nisnym@gmail.com>
2021-09-02 23:35:23 +0530

    Fn + Basic

--- CPP/STL/v1.cpp
+++ CPP/STL/v1.cpp
@@ -6,14 +6,15 @@ main(){
 
     int N, Q;
     cin >> N >> Q;
-    vector<int> seqList(N,0);
+    vector<int> seqList(N);
     for (int i = 0; i < seqList.size(); i++)
     {
         cout << seqList[i]<<endl;
     }
     for(int x : seqList)
         cout<< x << "\t";
-    cout<<Q;
+    cout<<Q<<endl;
+    cout<<12%4;
 
 
 }
