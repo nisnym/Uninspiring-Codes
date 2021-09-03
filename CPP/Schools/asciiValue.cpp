bcf1330ac52711a25288b3c04ef1fa63c6222e50
nisnym <nisnym@gmail.com>
2021-09-03 23:35:33 +0530

    Fn + Basic

---
+++ CPP/Schools/asciiValue.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+//main program
+int main()
+{
+	char val;
+	cout<<"Enter a character: ";
+	cin>>val;
+
+//printing the ASCII value of input 
+ //through typecasting
+
+cout<<"The ASCII value of "<<val<<" is "<<(int)val;
+return 0;
+}
