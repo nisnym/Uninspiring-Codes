d56eb6fcda9898a8d8cb4ea0bc82d1931ecc5f1b
nisnym <nisnym@gmail.com>
2021-09-17 22:29:07 +0530

    Fn

---
+++ CPP/TestCases/nothing.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+// #include<climits> also can be used <limits.h>
+
+using namespace std;
+int main(){
+	// int n;
+	// cout<<"ENter the numver to find the factorial"<<endl;
+	// cin >> n;
+	// int mini = INT_MIN;
+	// int maxi = INT_MAX;
+	// cout<<maxi << "\t" << mini;
+	return 0;
+}
